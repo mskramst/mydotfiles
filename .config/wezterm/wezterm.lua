f25d5310f54c0c61ca6be1cf3b5ecf6e1495caa0
mskramst <m@upgraded.lan>
2025-07-03 07:44:03 -0700

    updated wezterm settings

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	--window_decorations = "RESIZE",
+  color_scheme = 'Guezwhoz',
+	font_size = 18,
+}
+
+config.harfbuzz_features = { 'calt=0' }
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize() end)
+
+-- and finally, return the configuration to wezterm
+return config
